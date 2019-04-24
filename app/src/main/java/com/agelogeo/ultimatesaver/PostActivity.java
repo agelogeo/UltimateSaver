d3d0337feb5aa2c5b7cca30351891f42facc2110
agelogeo <agelogeo@gmail.com>
2019-04-24 15:55:41 +0300

    Post Activity updated. Back button added. Change the UI a bit.

--- app/src/main/java/com/agelogeo/ultimatesaver/PostActivity.java
+++ app/src/main/java/com/agelogeo/ultimatesaver/PostActivity.java
@@ -1,13 +1,23 @@
 package com.agelogeo.ultimatesaver;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -17,13 +27,38 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        VideoView videoView = findViewById(R.id.post_Video);
+        ImageView imageView = findViewById(R.id.post_Photo);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent i = getIntent();
         final int position = i.getIntExtra("position",0);
-        ImageView imageView = findViewById(R.id.post_Photo);
-        Picasso.get().setIndicatorsEnabled(true);
-        Picasso.get().load(SavedDownloads.getItemFromStaticDownloads(position).getLink()).into(imageView);
-        PhotoViewAttacher mAttacher = new PhotoViewAttacher(imageView);
+
+        setTitle("@"+SavedDownloads.getItemFromStaticDownloads(position).getUsername());
+
+
+        if(SavedDownloads.getItemFromStaticDownloads(position).isVideo) {
+            videoView.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.GONE);
+            try {
+                MediaController mediaController = new MediaController(this);
+                mediaController.setAnchorView(imageView);
+
+                videoView.setMediaController(mediaController);
+                videoView.setVideoURI(Uri.parse(SavedDownloads.getItemFromStaticDownloads(position).getLink()));
+                videoView.start();
+
+            }  catch ( Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            videoView.setVisibility(View.GONE);
+            imageView.setVisibility(View.VISIBLE);
+
+            Picasso.get().load(SavedDownloads.getItemFromStaticDownloads(position).getLink()).into(imageView);
+            //PhotoViewAttacher mAttacher = new PhotoViewAttacher(imageView);
+        }
 
         Button post_Save = findViewById(R.id.post_Save);
         post_Save.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +71,16 @@ public class PostActivity extends AppCompatActivity {
 
 
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
